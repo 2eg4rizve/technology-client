b69680b001e83887ac9a7e0a4906824b867e71d6
2eg4rizve <2eg4rizve.com>
2023-10-18 00:53:43 +0600

    BdetailsCard style

--- src/components/Brand/BdetailsCard.jsx
+++ src/components/Brand/BdetailsCard.jsx
@@ -1,14 +1,19 @@
+/* eslint-disable no-unused-vars */
 
 
-const BdetailsCard = (product = {product}) => {
+const BdetailsCard = ({ product }) => {
+
+    const { UserName, userEmail, photo, productName, bandName, type, price, shortDescription, rating } = product || {};
+
+
     return (
         <div>
 
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
                 <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title">{productName}</h2>
+                    <p>{bandName}</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
